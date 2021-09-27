f1c3a01adb58cff94689cfd4c4944948d22dbd1c
Karn Ratana <karn@ratana.info>
2021-09-27 01:43:21 +0000

    Add __template.js

---
+++ src/problems/__template.js
@@ -0,0 +1,28 @@
+// {url}
+/**
+ * @param {number} param1
+ * @return {number}
+ */
+ var myFunction = function(param1) {
+    return attempt1(param1);
+};
+
+// attempt1: description on the algorithm
+let attempt1 = (param1) => {
+    let startTime = Date.now();
+    console.log('  heapUsed 1: ', process.memoryUsage().heapUsed);
+
+    // do something
+    let result = param1;
+    
+    let endTime = Date.now();
+    console.log('  heapUsed 2: ', process.memoryUsage().heapUsed);
+    console.log('  Total time: ', endTime - startTime);
+    console.log('--------------------------')
+
+    return result;
+};
+
+let input = [1,2,3,4,5];
+let output = myFunction(input);
+console.log(output);
