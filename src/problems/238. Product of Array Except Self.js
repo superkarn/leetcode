ac30ed2ffa516f607cb7f7fc1862719d75a5bf45
Karn Ratana <karn@ratana.info>
2021-12-03 03:28:29 +0000

    Add left/right solution.

--- src/problems/238. Product of Array Except Self.js
+++ src/problems/238. Product of Array Except Self.js
@@ -7,11 +7,11 @@ var productExceptSelf = function(nums) {
     let startTime = Date.now();
     console.log('--------------------------');
     console.log('--Parameters--------------');
-    //console.log(`nums      :  ${nums}`);
+    console.log(`nums      :  ${nums}`);
 
     // Pick the algorithm
     console.log('--Logs--------------------');
-    let result = bruteForce(nums);
+    let result = leftRight(nums);
     
     let endTime = Date.now();
     console.log('--Stats-------------------');
@@ -36,6 +36,30 @@ let bruteForce = (nums) => {
             }
         }
         result.push(product);
+    }
+
+    return result;
+};
+
+// https://leetcode.com/problems/product-of-array-except-self/discuss/65622/Simple-Java-solution-in-O(n)-without-extra-space
+// Calculate the left products of each item; save in an array.
+// Calculate the right products of each item; save in another array.
+// Multiple left and right of each item; return the result.
+let leftRight = (nums) => {
+    let result = new Array(nums);
+    result[0] = 1;
+
+    // Calculate the left products of each item
+    for (let i=1; i<nums.length; i++) {
+        result[i] = result[i-1] * nums[i-1];
+    }
+    console.log(`left: ${result}`);
+
+    // Calculate the right products of each item
+    let right = 1;
+    for (let i=nums.length-1; i >= 0; i--) {
+        result[i] *= right;
+        right *= nums[i];
     }
 
     return result;
