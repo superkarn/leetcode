d17f6e922d259cde8491c2903ff3878b0c173eec
Karn Ratana <karn@ratana.info>
2021-12-12 03:59:19 +0000

    Add binary search

--- src/problems/153. Find Minimum in Rotated Sorted Array.js
+++ src/problems/153. Find Minimum in Rotated Sorted Array.js
@@ -11,7 +11,7 @@ var findMin = function(param1) {
 
     // Pick the algorithm
     console.log('--Logs--------------------');
-    let result = bruteForce(param1);
+    let result = binarySplit(param1);
     
     let endTime = Date.now();
     console.log('--Stats-------------------');
@@ -43,8 +43,45 @@ let bruteForce = (param1) => {
     throw new Error('Something went wrong.');
 };
 
-let input = [1];
+let binarySplit = (param1) => {
+    //console.log(`length: ${param1.length}`);
+    //console.log(param1);
+    if (param1.length == 0) return [];
+    if (param1.length == 1) return param1[0];
+    if (param1.length == 2) return Math.min(...param1);
+
+
+    let l = Math.floor(param1.length / 3);
+    let r = 2*l;
+
+    //console.log(`  l[${l}]: ${param1[l]}, r[${r}]: ${param1[r]}`);
+
+    // make a copy
+    let p = param1.slice();
+
+    // if l<r, then the min is not between l and r 
+    // and we know r is not the minimum, but l could still be.
+    // so we keep l, but not r
+    if (param1[l] < param1[r]) {
+        //console.log(`  outside`);
+        p = [...param1.slice(0, l+1), ...param1.slice(r+1, param1.length)];
+    } 
+    // else min is between l and r, trim the rest.
+    // and we know l is not the minimum, but r could still be.
+    // keep both r but not l
+    else {
+        //console.log(`  inside`);
+        p = param1.slice(l+1, r+1);
+    }
+
+    return binarySearch(p);
+};
+
+let input = [1]; // 1
+//let input = [1,2]; // 1
+//let input = [3,1,2]; // 1
 //let input = [3,4,5,1,2]; // 1
 //let input = [4,5,6,7,0,1,2]; // 0
 //let input = [11,13,15,17]; // 11
+//let input = [6,7,8,9,10,1,2,3,4,5]; // 1
 let output = findMin(input);
