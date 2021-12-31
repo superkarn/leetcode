c0accc4a1466f20fb98422ffdfb5321c94a65130
Karn Ratana <karn@ratana.info>
2021-12-31 04:08:09 +0000

    Add dynamic programming method

--- src/problems/62. Unique Paths.js
+++ src/problems/62. Unique Paths.js
@@ -4,15 +4,16 @@
  * @param {number} n
  * @return {number}
  */
-var uniquePaths = function(param1) {
+var uniquePaths = function(m, n) {
     let startTime = Date.now();
     console.log('--------------------------');
     console.log('--Parameters--------------');
-    console.log(`param1    :  ${param1}`);
+    console.log(`m         :  ${m}`);
+    console.log(`n         :  ${n}`);
 
     // Pick the algorithm
     console.log('--Logs--------------------');
-    let result = bruteForce(param1);
+    let result = dp(m, n);
     
     let endTime = Date.now();
     console.log('--Stats-------------------');
@@ -27,13 +28,31 @@ var uniquePaths = function(param1) {
 };
 
 // attempt1: description on the algorithm
-let bruteForce = (param1) => {
-    let result = param1;
+let dp = (m, n) => {
+    // a cache of m x n
+    let cache = Array(m).fill(Array(n));
 
-    return result;
+    // all the m x 1 are 1s
+    for (let i_m=0; i_m<m; i_m++) {
+        cache[i_m][0] = 1;
+    }
+
+    // all the 1 x n are 1s
+    for (let i_n=0; i_n<n; i_n++) {
+        cache[0][i_n] = 1;
+    }
+
+    for (let i_m=1; i_m<m; i_m++) {
+        for (let i_n=1; i_n<n; i_n++) {
+            cache[i_m][i_n] = cache[i_m-1][i_n] + cache[i_m][i_n-1];
+            //console.log(`[${i_m}][${i_n}] = ${cache[i_m][i_n]}`);
+        }
+    }
+
+    return cache[m-1][n-1];
 };
 
-let m = 3, n = 7; // 28
-//let m = 3, n = 2; // 3
+//let m = 3, n = 7; // 28
+let m = 3, n = 2; // 3
 
 let output = uniquePaths(m, n);
