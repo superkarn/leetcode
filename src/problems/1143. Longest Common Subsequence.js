f90f09aae8e823adb7f0c7c87d7e3e268de220c8
Karn Ratana <karn@ratana.info>
2022-01-07 04:37:21 +0000

    Added a DP solution from https://www.youtube.com/watch?v=NnD96abizww

--- src/problems/1143. Longest Common Subsequence.js
+++ src/problems/1143. Longest Common Subsequence.js
@@ -13,7 +13,7 @@ var longestCommonSubsequence = function(text1, text2) {
 
     // Pick the algorithm
     console.log('--Logs--------------------');
-    let result = bruteForce(text1, text2);
+    let result = dp(text1, text2);
     
     let endTime = Date.now();
     console.log('--Stats-------------------');
@@ -27,7 +27,8 @@ var longestCommonSubsequence = function(text1, text2) {
     return result;
 };
 
-// attempt1: description on the algorithm
+// self attempt
+// Not 100% working
 let bruteForce = (text1, text2) => {
     let t1 = [], t2 = [];
     let r1 = [], r2 = [];
@@ -94,11 +95,46 @@ let bruteForce = (text1, text2) => {
     return r1.filter(x => x).length;
 };
 
+// https://www.youtube.com/watch?v=NnD96abizww
+let dp = (text1, text2) => {
+    // data is a 2 dimentional array, storing the longest common subsequence (LCS)
+    // up to text[1] and text[2].  The answer is in the "bottom right" corner of the array.
+    // 
+    // Do not use Array.fill() to create multi-dimensional arrays.
+    // https://stackoverflow.com/a/47057799/1398750
+    let data = []; //Array(text1.length+1).fill(Array(text2.length+1).fill(0));
+    for (let i = 0; i<text1.length+1; i++) {
+        data[i] = [];
+        for (let j=0; j<text2.length+1; j++) {
+            data[i][j] = 0;
+        }
+    }
+
+    for (let i=0; i<text1.length; i++) {
+        let di = i+1;
+        for (let j=0; j<text2.length; j++) {
+            let dj = j+1;
+            if (text1[i] == text2[j]) {
+                data[di][dj] = data[di-1][dj-1] + 1;
+                //console.log(`  MATCH ${text1[i]}==${text2[j]}: [${di}][${dj}] = ${data[di][dj]} from [${di-1}][${dj-1}] (${data[di-1][dj-1]})`);
+            } else {
+                data[di][dj] = Math.max(data[di][dj-1], data[di-1][dj]); 
+                //console.log(`  SKIP  ${text1[i]}!=${text2[j]}: [${di}][${dj}] = ${data[di][dj]}`);
+            }
+        }
+        //console.log();
+    }
+    
+    return data[text1.length][text2.length];
+};
+
+//let text1 = "xabccde", text2 = "ace"; // 3
 //let text1 = "abcde", text2 = "ace"; // 3
 //let text1 = "abc", text2 = "abc"; // 3
 //let text1 = "abc", text2 = "def"; // 0
 //let text1 = "abcde", text2 = "aec"; // 2
 //let text1 = "bsbininm", text2 = "jmjkbkjkv"; // 1
-let text1 = "oxcpqrsvwf", text2 = "shmtulqrypy"; // 2
+//let text1 = "oxcpqrsvwf", text2 = "shmtulqrypy"; // 2
+//let text1 = "abcba", text2 = "abcbcba"; // 5
 
 let output = longestCommonSubsequence(text1, text2);
