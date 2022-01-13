a6d8e42e964c5aa39757e33bd6ca483e6ef781e3
Karn Ratana <karn@ratana.info>
2022-01-13 03:25:06 +0000

    Add brute force

--- src/problems/28. Implement strStr().js
+++ src/problems/28. Implement strStr().js
@@ -29,11 +29,33 @@ var strStr = function(haystack, needle) {
 
 // attempt1: description on the algorithm
 let bruteForce = (haystack, needle) => {
-    
+    if (needle == '') return 0;
+    if (needle.length > haystack.length) return -1;
+
+    for (let i=0; i<haystack.length-needle.length+1; i++) {
+        let goToNextI = false;
+        //console.log(`  ${i}`);
+        for (let j=0; j<needle.length && !goToNextI; j++) {
+            if (haystack[i+j] != needle[j]) {
+                //console.log(`    [${i+j}] (${haystack[i+j]}) != (${needle[j]}), go to next i`);
+                goToNextI = true;
+                break;
+            }
+        }
+
+        if (!goToNextI) {
+            console.log(`    found it at ${i}`);
+            return i;
+        }
+    }
+
+    // Not found
+    return -1;
 };
 
-let haystack = "hello", needle = "ll"; // 2
+//let haystack = "hello", needle = "ll"; // 2
 //let haystack = "aaaaa", needle = "bba"; // -1
 //let haystack = "", needle = ""; // 0
+let haystack = "hello world!", needle = "o w"; // 4
 
 let output = strStr(haystack, needle);
