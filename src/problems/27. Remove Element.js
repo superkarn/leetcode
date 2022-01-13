abca11804b2bd07b43981617281fbcf6dbf2d400
Karn Ratana <karn@ratana.info>
2022-01-13 02:52:53 +0000

    Add brute force solution

--- src/problems/27. Remove Element.js
+++ src/problems/27. Remove Element.js
@@ -29,12 +29,34 @@ var removeElement = function(nums, val) {
 
 // attempt1: description on the algorithm
 let bruteForce = (nums, val) => {
-    let result = param1;
+    let uniqueNumbers = nums.length;
 
-    return result;
+    for (let i=0; i<uniqueNumbers; i++) {
+        //console.log(`   nums[${i}] (${nums[i]})`);
+
+        if (nums[i] == val) {
+            //console.log(`      ${nums}`);
+            //console.log(`      shifting`);
+            for (let j=i; j<nums.length; j++) {
+                nums[j] = nums[j+1];
+            }
+            nums[nums.length-1] = val;
+            //console.log(`      ${nums}`);
+
+            // Since we just moved i to the end, check i again
+            // whicih should now has the next value
+            i--;
+
+            // Also decrement the loop termination index
+            uniqueNumbers--;
+        }
+    }
+    
+    // Kind of cheating using filter()
+    return nums.filter(x => x!=val).length;
 };
 
-let nums = [3,2,2,3], val = 3; // 2
-//let nums = [0,1,2,2,3,0,4,2], val = 2; // 5
+//let nums = [3,2,2,3], val = 3; // 2
+let nums = [0,1,2,2,3,0,4,2], val = 2; // 5
 
 let output = removeElement(nums, val);
