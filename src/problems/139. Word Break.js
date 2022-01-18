5b5f056df3f1dc7e9571aa302122c70662cc70fa
Karn Ratana <karn@ratana.info>
2022-01-18 01:36:01 +0000

    Reset to boiler plate

--- src/problems/139. Word Break.js
+++ src/problems/139. Word Break.js
@@ -4,7 +4,7 @@
  * @param {string[]} wordDict
  * @return {boolean}
  */
-var wordBreak = function(s, wordDict) {
+ var wordBreak = function(s, wordDict) {
     let startTime = Date.now();
     console.log('--------------------------');
     console.log('--Parameters--------------');
@@ -29,44 +29,15 @@ var wordBreak = function(s, wordDict) {
 
 // attempt1: description on the algorithm
 let bruteForce = (s, wordDict) => {
-    // This regex is used to remove `_`
-    let regexPlaceHolder = /_/ig;
+    let result = s;
 
-    for (let i=0; i<wordDict.length; i++) {
-        let s2 = s;
-        for (let j=i; j<wordDict.length; j++) {
-            let w = wordDict[j];
-
-            // Set up Regular Expression to remove the word from the string
-            let pattern = w;
-            let regex = new RegExp(pattern, 'i');
-
-            //console.log(`  ${i}: ${w}: ${regex}`);
-            
-            // If word is found, keep removing it from the string
-            while (s2.indexOf(w) > -1) {
-                //console.log(`    Removing ${w}`);
-                s2 = s2.replace(regex, '_');
-            }
-        }
-
-        // If the string (without '_') is empty, return true
-        // else continue to the next i loop
-        if (s2.replace(regexPlaceHolder, '').length == 0) {
-            return true;
-        }
-    }
-
-    return false;
+    return result;
 };
 
-//let s = "leetcode", wordDict = ["leet","code"]; // true
-//let s = "applepenapple", wordDict = ["apple","pen"]; // true
-//let s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]; // false
-//let s = "aaa", wordDict = ["aaa"]; // true
-//let s = "aaa", wordDict = ["bbb"]; // false
-//let s = "cars", wordDict = ["car","ca","rs"]; // true
-//let s = "ccbb", wordDict = ["bc","cb"]; // false
-let s = "ccaccc", wordDict = ["cc","ac"]; // true
+let inputs = [
+    { s:"leetcode", wordDict:["leet","code"], output:true },
+    { s:"applepenapple", wordDict:["apple","pen"], output:true },
+];
 
-let output = wordBreak(s, wordDict);
+// Comparing scalar output
+console.log(inputs.map(x => wordBreak(x.s, x.wordDict) == x.output));
