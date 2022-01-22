017a344bc75ff2a8e0dfd2f93a72123492a39ceb
Karn Ratana <karn@ratana.info>
2022-01-22 16:28:23 +0000

    Add DP solution

--- src/problems/213. House Robber II.js
+++ src/problems/213. House Robber II.js
@@ -11,7 +11,7 @@ var rob = function(nums) {
 
     // Pick the algorithm
     console.log('--Logs--------------------');
-    let result = bruteForce(nums);
+    let result = dp(nums);
     
     let endTime = Date.now();
     console.log('--Stats-------------------');
@@ -25,17 +25,46 @@ var rob = function(nums) {
     return result;
 };
 
-// attempt1: description on the algorithm
-let bruteForce = (nums) => {
-    let result = nums;
+// DP, modified from #198
+let dp = (nums) => {
+    if (nums.length == 0) return 0;
+    if (nums.length == 1) return nums[0];
+    
+    return Math.max(
+            rob1(nums.slice(1)), 
+            rob1(nums.slice(0,nums.length-1))
+    );
+};
 
-    return result;
+let rob1 = (nums) => {
+    //console.log(`  nums: ${nums}`);
+
+    // Special cases
+    if (nums.length == 0) return 0;
+
+    let cache1 = 0; // dp[i-1]
+    let cache2 = 0; // dp[i-2]
+
+    for (let i=0; i<nums.length-1; i++) {
+        let tmp = cache1;
+        cache1 = Math.max(cache2+nums[i], cache1);
+        cache2 = tmp;
+        //console.log(`  cache1 = ${cache1}, cache2 = ${cache2}`);
+    }
+
+    // Check if the last one conflicts with the first
+    cache1 = Math.max(cache2+nums[nums.length-1], cache1);
+
+    //console.log(`    result: ${cache1}`);
+    return cache1;
 };
 
 let inputs = [
-    { nums:[2,3,2], output:3 },
-    { nums:[1,2,3,1], output:4 },
-    { nums:[1,2,3], output:3 },
+    { nums:[], output:0 },
+    { nums:[1], output:1 },
+    //{ nums:[2,3,2], output:3 },
+    //{ nums:[1,2,3,1], output:4 },
+    //{ nums:[1,2,3], output:3 },
 ];
 
 // Comparing scalar output
