088f2495819c7dd0553aeb608155ab2a4ea0dce7
Karn Ratana <karn@ratana.info>
2022-01-22 15:21:00 +0000

    Add DP solution

--- src/problems/198. House Robber.js
+++ src/problems/198. House Robber.js
@@ -11,7 +11,7 @@ var rob = function(nums) {
 
     // Pick the algorithm
     console.log('--Logs--------------------');
-    let result = bruteForce(nums);
+    let result = dp(nums);
     
     let endTime = Date.now();
     console.log('--Stats-------------------');
@@ -25,16 +25,37 @@ var rob = function(nums) {
     return result;
 };
 
-// attempt1: description on the algorithm
-let bruteForce = (nums) => {
-    let result = nums;
+// Dynamic Programming
+//  cache[i] = nums[i] + max(cache[i-2], cache[i-3])
+//  result = cache[nums.lenght-1]
+let dp = (nums) => {
+    // Special cases
+    if (nums.length == 0) return 0;
+    if (nums.length == 1) return nums[0];
+    if (nums.length == 2) return Math.max(...nums);
+    
+    let cache = [];
 
-    return result;
+    // Caculate the first 3 for optimization
+    cache[0] = nums[0];
+    cache[1] = Math.max(nums[0], nums[1]);
+    cache[2] = Math.max(nums[0], nums[1], nums[2], nums[0]+nums[2]);
+
+    for (let i=3; i<nums.length; i++) {
+        cache[i] = nums[i] + Math.max(cache[i-2], cache[i-3]);
+        //console.log(`  cache[${i}] = ${cache[i]} = ${nums[i]} + Math.max(${cache[i-2]}, ${cache[i-3]})`);
+    }
+
+    return Math.max(...cache);
 };
 
 let inputs = [
+    { nums:[], output:0 },
+    { nums:[2], output:2 },
+    { nums:[2,3], output:3 },
     { nums:[1,2,3,1], output:4 },
     { nums:[2,7,9,3,1], output:12 },
+    { nums:[1,3,1,3,100], output:103 },
 ];
 
 // Comparing scalar output
