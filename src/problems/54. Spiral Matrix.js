f29ff2d4739e0d18047b74f55b4e83cd14dd574a
Karn Ratana <karn@ratana.info>
2022-02-01 04:03:16 +0000

    Add spiral traversal

--- src/problems/54. Spiral Matrix.js
+++ src/problems/54. Spiral Matrix.js
@@ -11,7 +11,7 @@ var spiralOrder = function(matrix) {
 
     // Pick the algorithm
     console.log('--Logs--------------------');
-    let result = matrixRotation(matrix);
+    let result = spiralTraversal(matrix);
     
     let endTime = Date.now();
     console.log('--Stats-------------------');
@@ -54,6 +54,66 @@ class MatrixHelper {
         return matrix[0].map((val, index) => matrix.map(row => row[row.length-1-index]));
     };
 }
+
+let spiralTraversal = (matrix) => {
+    // Special cases
+
+    let result = [];
+
+    // Right: [0,1]
+    // Down: [1,0]
+    // Left: [0,-1]
+    // Up: [-1,0]
+    let directions = [[0,1], [1,0], [0,-1], [-1,0]];
+
+    // Direction increment % 4
+    let i_dir = 0;
+
+    // Horizontal position
+    let x = -1;
+
+    // Vertical position
+    let y = 0;
+
+    let horizontalRange = matrix[0].length;
+    let verticalRange = matrix.length-1;
+
+    //console.log(`${MatrixHelper.toString(matrix)}`);
+  
+    while ((i_dir%2==0 ? horizontalRange : verticalRange)>0) {
+        let currentDirection = directions[i_dir];
+        
+        //console.log(`position:   [${y},${x}]`);
+        //console.log(`i_dir:      ${i_dir}, [${currentDirection}]`);
+        //console.log(`horizontal: ${horizontalRange}`);
+        //console.log(`vertical:   ${verticalRange}`);
+
+
+        for (let i=0; i < (i_dir%2==0 ? horizontalRange : verticalRange); i++) {
+            y += directions[i_dir][0];
+            x += directions[i_dir][1];
+            //console.log(`  [${y},${x}] = ${matrix[y][x]}`);
+            result.push(matrix[y][x]);
+        }
+        
+
+        // If direction is horizontal, then decrease horizontalRange
+        if (i_dir % 2 == 0) {
+            horizontalRange--;
+        } 
+        // Else direction is vertical, decrease verticalRange
+        else {
+            verticalRange--;
+        }
+
+        // Change direction
+        i_dir = (i_dir + 1) % 4;
+
+        //console.log(``);
+    }
+
+    return result;
+};
 
 let inputs = [
     { matrix:[[1,2,3],[4,5,6],[7,8,9]], output:[1,2,3,6,9,8,7,4,5] },
