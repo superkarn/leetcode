a9d8c0aff821c1fa5f695cb84e434b512f6ade22
Karn Ratana <karn@ratana.info>
2022-02-02 13:58:04 +0000

    Add "optimized" solution

--- src/problems/73. Set Matrix Zeroes.js
+++ src/problems/73. Set Matrix Zeroes.js
@@ -69,9 +69,78 @@ let updateMarkedCells = (matrix, newMark=0) => {
 // Use this to mark cells that should be turned to 0 vs started off as 0
 const MARK = 'x';
 
+// https://www.youtube.com/watch?v=T41rL0L3Pnw
+let optimized = (matrix) => {
+
+    //console.log('1 original\n'+ MatrixHelper.toString(matrix));
+
+    let firstColumnZero = false;
+
+    for (let r=0; r<matrix.length; r++) {
+        if (matrix[r][0] == 0) {
+            firstColumnZero = true;
+        }
+        
+        for (let c=1; c<matrix[r].length; c++) {
+            // If the current cell is 0, mark the first row/column
+            if (matrix[r][c] == 0) {
+                matrix[0][c] = 0;
+                matrix[r][0] = 0;
+            }
+        }
+    }
+    //console.log('2 marked 1st row/col\n'+ MatrixHelper.toString(matrix));
+
+    // Update the rows 1 -> n-1
+    for (let r=1; r<matrix.length; r++) {
+        if (matrix[r][0] == 0) {
+            for (let c=0; c<matrix[r].length; c++) {
+                matrix[r][c] = 0;
+            }
+        }
+    }
+    //console.log('3 zeroed out rows 1->n-1\n'+ MatrixHelper.toString(matrix));
+
+    // Update the columns 1 -> n-1
+    for (let c=1; c<matrix[0].length; c++) {
+        if (matrix[0][c] == 0) {
+            for (let r=0; r<matrix.length; r++) {
+                matrix[r][c] = 0;
+            }
+        }
+    }
+
+    // Update the first row
+    if (matrix[0][0] == 0) {
+        for (let c=0; c<matrix[0].length; c++) {
+            matrix[0][c] = 0;
+        }
+    }
+
+    // Update the first column
+    if (firstColumnZero == true) {
+        for (let r=0; r<matrix.length; r++) {
+            matrix[r][0] = 0;
+        }
+    }
+
+    //console.log('4 zeroed out cols\n'+ MatrixHelper.toString(matrix));
+
+    return matrix;
+};
+class MatrixHelper {
+    // Return a human-friendly string representation of a n x m matrix
+    static toString = (matrix) => {
+        return matrix.reduce((prev, curr) => prev + curr + '\r\n', '');
+    };
+}
+
 let inputs = [
     { matrix:[[1,1,1],[1,0,1],[1,1,1]], output:[[1,0,1],[0,0,0],[1,0,1]] },
     { matrix:[[0,1,2,0],[3,4,5,2],[1,3,1,5]], output:[[0,0,0,0],[0,4,5,0],[0,3,1,0]] },
+    { matrix:[[1,1,2,2],[3,0,0,2],[1,3,1,5]], output:[[1,0,0,2],[0,0,0,0],[1,0,0,5]] },
+    { matrix:[[1,1,2,2],[0,1,1,2],[1,3,1,5]], output:[[0,1,2,2],[0,0,0,0],[0,3,1,5]] },
+    { matrix:[[1,0,2,2],[1,1,1,2],[1,3,1,5]], output:[[0,0,0,0],[1,0,1,2],[1,0,1,5]] },
     { matrix:[[-1],[2],[3]], output:[[-1],[2],[3]] },
 ];
 
