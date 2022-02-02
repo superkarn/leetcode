8c897148cdf34f9eb8db781934db08041d5cf193
Karn Ratana <karn@ratana.info>
2022-02-02 14:07:35 +0000

    Update __template.js, update array testing

--- src/problems/__template.js
+++ src/problems/__template.js
@@ -41,4 +41,22 @@ let inputs = [
 console.log(inputs.map(x => myFunction(x.param1) == x.output));
 
 // Comparing array output
-console.log(inputs.map(x => myFunction(x.digits).every((v,i) => v === x.output[i])));
+console.log(inputs.map(x => myFunction(x.param1).every((v,i) => v === x.output[i])));
+console.log(inputs.map(x => {
+    let results = setZeroes(x.matrix)
+    if (x.output.length != results.length) return false;
+
+    return v === x.output[i];
+}));
+
+// Comparing 2D array output
+// Comparing 2D array output
+console.log(inputs.map(x => {
+    let results = setZeroes(x.matrix)
+    if (x.output.length != results.length) return false;
+
+    return results.every((v,i) => {
+        if (v.length != x.output[i].length) return false;
+        return v.every((v2,i2) => v2 === x.output[i][i2])
+    });
+}));
