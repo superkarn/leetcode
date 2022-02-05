920a313dd0b2912cb1cd87dca2d42a5cec7040e6
Karn Ratana <karn@ratana.info>
2022-02-04 22:45:59 -0500

    Update problem 3 with a better solution using sliding window. (#39)

--- src/problems/3. Longest Substring Without Repeating Characters.js
+++ src/problems/3. Longest Substring Without Repeating Characters.js
@@ -1,48 +1,70 @@
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/
 /**
- * @param {number} param1
+ * @param {string} s
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    return attempt1(s);
-};
+    let startTime = Date.now();
+    console.log('--------------------------');
+    console.log('--Parameters--------------');
+    console.log(`s         :  ${s}`);
 
-// attempt1: description on the algorithm
-let attempt1 = (s) => {
-    if (!s) return 0;
+    // Pick the algorithm
+    console.log('--Logs--------------------');
+    let result = bruteForce(s);
     
-    let result = 0;
-    
-    for (let ii = 0; ii < s.length; ii++) {
-        let tmp = helper(s.slice(ii, s.length));
-        if (tmp > result) result = tmp;
-    }
-    
+    let endTime = Date.now();
+    console.log('--Stats-------------------');
+    console.log('Memory    : ', process.memoryUsage().heapUsed);
+    console.log('Total time: ', endTime - startTime);
+
+    console.log('--Result------------------');
+    console.log(result);
+    console.log('--------------------------');
+
     return result;
 };
 
-let helper = (s) => {
+// attempt1: description on the algorithm
+let bruteForce = (s) => {
+    // Special cases
     if (!s) return 0;
+    if (s.length == 0) return 0;
+    if (s.length == 1) return 1;
+
+    let l = 0;
+    let r = 1;
+    let maxLength = 0;
     
-    let result = 0;
-    
-    for (let jj = 0; jj < s.length; jj++) {
-        result++;
-        
-        // if the current character already exist in the substring
-        // then return the length
-        if (s.substring(0, jj).indexOf(s[jj]) > -1) {
-            return jj;
+    while (r < s.length) {
+        let sub = s.substring(l,r);
+        //console.log(`  (${l},${r}) -> ${sub}`);
+
+        // If the new char s[r] is already in the current substring, 
+        // then it's a repeating char.  We move l up.
+        // Can be optimized by moving l up to the last indexOf +1
+        if (sub.indexOf(s[r]) > -1) {
+            //console.log(`    sub.indexOf(${s[r]}): ${sub.indexOf(s[r])}`);
+            l++;
+        }
+        // else it's not a repeating char.  We move r up 1, and check maxLength
+        else {
+            maxLength = Math.max(maxLength, r-l+1);
+            //console.log(`    maxLength: ${maxLength}`);
+            r++;
         }
     }
-    
-    return s.length;
+
+    return maxLength;
 };
 
-let input = "abcabcbb";
-//let input = "bbbbb";
-//let input = "pwwkew";
-//let input = "";
+let inputs = [
+    { s:"abcabcbb", output:3 },
+    { s:"a", output:1 },
+    { s:"bbbbb", output:1 },
+    { s:"pwwkew", output:3 },
+    { s:"au", output:2 },
+];
 
-let output = lengthOfLongestSubstring(input);
-console.log(output);
+// Comparing scalar output
+console.log(inputs.map(x => lengthOfLongestSubstring(x.s) == x.output));
