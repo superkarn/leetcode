4b44da4a51d05d65952ce6d5b02cdd64353b988a
Karn Ratana <karn@ratana.info>
2022-02-06 22:17:07 -0500

    Add sliding window solution

--- src/problems/424. Longest Repeating Character Replacement.js
+++ src/problems/424. Longest Repeating Character Replacement.js
@@ -13,7 +13,7 @@ var characterReplacement = function(s, k) {
 
     // Pick the algorithm
     console.log('--Logs--------------------');
-    let result = bruteForce(s, k);
+    let result = slidingWindow(s, k);
     
     let endTime = Date.now();
     console.log('--Stats-------------------');
@@ -27,9 +27,40 @@ var characterReplacement = function(s, k) {
     return result;
 };
 
-// attempt1: description on the algorithm
-let bruteForce = (s, k) => {
-    let result = s;
+// Use sliding window
+// https://www.youtube.com/watch?v=gqXU1UyA8pk
+let slidingWindow = (s, k) => {
+    let result = 0;
+
+    // This stores the count of each letter in the current substring
+    let letterCount = {};
+    let maxFrequency = 0;
+    let l = 0;
+
+    for (r=0; r<s.length; r++) {
+        let subStringLength = r-l+1;
+        //console.log(`----- ${s.substring(l,r+1)}, length: ${subStringLength}`);
+
+        letterCount[s[r]] = 1 + (letterCount[s[r]] ? letterCount[s[r]] : 0);
+        //console.log(`    new letter s[${r}] (${s[r]}) -> ${letterCount[s[r]]}`);
+    
+        maxFrequency = Math.max(maxFrequency, letterCount[s[r]]);
+
+        //console.log(`    ${subStringLength} - ${maxFrequency} > ${k}`);
+        // While we do not have enough substitution for the current string to become single char,
+        // move the left side up
+        while (subStringLength - maxFrequency > k) {
+            letterCount[s[l]]--;
+            l++;            
+            subStringLength = r-l+1;
+            //console.log(`    moved left to ${l}`);
+        }
+
+        result = Math.max(result, subStringLength);
+        //console.log(`    maxFrequency = ${maxFrequency}, result = ${result}`);
+    }
+    
+    //console.table(letterCount);
 
     return result;
 };
