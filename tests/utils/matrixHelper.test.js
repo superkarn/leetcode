53839b4496b7bcd4c7a669efcea1b9d613829595
Karn Ratana <karn@ratana.info>
2022-02-10 10:44:18 -0500

    Remove jest-each

--- tests/utils/matrixHelper.test.js
+++ tests/utils/matrixHelper.test.js
@@ -1,13 +1,13 @@
 const MatrixHelper = require('../../src/utils/matrixHelper');
-const each = require('jest-each').default;
 
 describe('MatrixHelper class', () => {
 
-    each([
-        [[], 3, 3, 0],
-        [[], 3, 4, -1],
-        [[], 4, 3, null]
-    ]).test('Filling a matrix with a default value', (matrix, m, n, val)  => {
+    test.each([
+        {matrix:[], m:3, n:3, val:0},
+        {matrix:[], m:3, n:4, val:-1},
+        {matrix:[], m:4, n:3, val:null},
+        {matrix:[], m:0, n:0, val:0},
+    ])('Filling a matrix with a default value', ({matrix, m, n, val})  => {
         // Arrange
         // See parameters
     
@@ -22,54 +22,38 @@ describe('MatrixHelper class', () => {
         }
     });
     
-    test('Rotate a matrix clockwise', ()  => {
+    test.each([
+        {matrix:[[1,2,3],[4,5,6],[7,8,9]], expected:[[7,4,1],[8,5,2],[9,6,3]]},
+        {matrix:[[1,2,3],[4,5,6],[7,8,9],[10,11,12]], expected:[[10,7,4,1],[11,8,5,2],[12,9,6,3]]},
+    ])('Rotate a matrix clockwise', ({matrix, expected})  => {
         // Arrange
-        let matrix = [
-            [1,2,3],
-            [4,5,6],
-            [7,8,9],
-            [10,11,12]
-        ];
-    
-        let rotatedMatrix = [
-            [10, 7, 4, 1],
-            [11, 8, 5, 2],
-            [12, 9, 6, 3]
-        ]
+        // See parameters
     
         // Act
         matrix = MatrixHelper.rotateClockWise(matrix);
     
         // Assert
-        for (let row=0; row<rotatedMatrix.length; row++) {
-            for(let col=0; col<rotatedMatrix[0].length; col++) {
-                expect(matrix[row][col]).toBe(rotatedMatrix[row][col]);
+        for (let row=0; row<expected.length; row++) {
+            for(let col=0; col<expected[0].length; col++) {
+                expect(matrix[row][col]).toBe(expected[row][col]);
             }
         }
     });
     
-    test('Rotate a matrix counter clockwise', ()  => {
+    test.each([
+        {matrix:[[1,2,3],[4,5,6],[7,8,9]], expected:[[3,6,9],[2,5,8],[1,4,7]]},
+        {matrix:[[1,2,3],[4,5,6],[7,8,9],[10,11,12]], expected:[[3,6,9,12],[2,5,8,11], [1,4,7,10]]},
+    ])('Rotate a matrix counter clockwise', ({matrix, expected})  => {
         // Arrange
-        let matrix = [
-            [1,2,3],
-            [4,5,6],
-            [7,8,9],
-            [10,11,12]
-        ];
-    
-        let rotatedMatrix = [
-            [3, 6, 9, 12],
-            [2, 5, 8, 11],
-            [1, 4, 7, 10]
-        ]
+        // See parameters
     
         // Act
         matrix = MatrixHelper.rotateCounterClockWise(matrix);
     
         // Assert
-        for (let row=0; row<rotatedMatrix.length; row++) {
-            for(let col=0; col<rotatedMatrix[0].length; col++) {
-                expect(matrix[row][col]).toBe(rotatedMatrix[row][col]);
+        for (let row=0; row<expected.length; row++) {
+            for(let col=0; col<expected[0].length; col++) {
+                expect(matrix[row][col]).toBe(expected[row][col]);
             }
         }
     });
