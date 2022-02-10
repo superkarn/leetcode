ffc47b8dac387370abc8e0c6e046ed039e275b26
Karn Ratana <karn@ratana.info>
2022-02-09 22:57:04 -0500

    Add a solution that counts frequencies

--- src/problems/242. Valid Anagram.js
+++ src/problems/242. Valid Anagram.js
@@ -13,7 +13,7 @@ var isAnagram = function(s, t) {
 
     // Pick the algorithm
     console.log('--Logs--------------------');
-    let result = bruteForce(s, t);
+    let result = countFrequencies(s, t);
     
     let endTime = Date.now();
     console.log('--Stats-------------------');
@@ -43,9 +43,49 @@ let bruteForce = (s,t) => {
     return true;
 };
 
+// Keep track of the frequencies of each letter in s,
+// then compare to t
+let countFrequencies = (s, t) => {
+    let freq = {};
+
+    // Count the frequencies in s
+    for (let i=0; i<s.length; i++) {
+        freq[s[i]] = 1 + (freq[s[i]] ? freq[s[i]] : 0);
+    }
+    
+    // Decrement the frequencies from t
+    for (let i=0; i<t.length; i++) {
+        // If a letter in t is not in s, return false
+        if (!freq[t[i]]) {
+            //console.log(`t has '${t[i]}', but s doesn't`);
+            return false;
+        }
+
+        freq[t[i]]--;
+
+        // If any letter drops below 0, return false
+        if (freq[t[i]] < 0) {
+            //console.log(`t has too many '${t[i]}'`);
+            return false;
+        }
+    }
+
+    // Make sure all frequencies end up with 0
+    for (let i in freq) {
+        if (freq[i] != 0) {
+            //console.log(`s has too many '${i}'`);
+            return false;
+        }
+    }
+
+    //console.table(freq);
+    return true;
+};
+
 let inputs = [
     { s:"anagram", t:"nagaram", output:true },
     { s:"rat", t:"car", output:false },
+    { s:"ab", t:"a", output:false },
 ];
 
 // Comparing scalar output
