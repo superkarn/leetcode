7dd5c75e5637a4a654fb0d09eb38dbd83493ebc5
Karn Ratana <karn@ratana.info>
2022-02-19 23:09:53 -0500

    Add ListNode.toString()

--- src/models/ListNode.js
+++ src/models/ListNode.js
@@ -3,6 +3,22 @@ class ListNode {
         this.val = (val===undefined ? 0 : val)
         this.next = (next===undefined ? null : next)
     };
+
+    toString = () => {
+        let str = `${this.val}`;
+        let n = this.next;
+
+        if (n) {
+            while (n.next) {
+                str += `, ${n.val}`;
+                n = n.next;
+            }
+
+            // The last node            
+            str += `, ${n.val}`;
+        }
+        return str;
+    }
 }
 
 module.exports = ListNode;
