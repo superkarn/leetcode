0bd8113e6b9657ac7210283b1bfd693b4066e759
Karn Ratana <karn@ratana.info>
2022-02-20 23:54:51 -0500

    Clean up tests

--- tests/utils/linkedListHelper.test.js
+++ tests/utils/linkedListHelper.test.js
@@ -2,6 +2,21 @@ const ListNode = require('../../src/models/ListNode');
 const LinkedListHelper = require('../../src/utils/linkedListHelper');
 
 describe('LinkedListHelper class', () => {
+    
+    test.each([
+        {head:[], expected:null},
+        {head:[1], expected:new ListNode(1, null)},
+        {head:[1,2], expected:new ListNode(1, new ListNode(2, null))},
+        {head:[1,2,3], expected:new ListNode(1, new ListNode(2,  new ListNode(3, null)))},
+    ])('LinkedListHelper.fromArray()', ({head, expected})  => {
+        // Arrange
+    
+        // Act
+        let result = LinkedListHelper.fromArray(head);
+    
+        // Assert
+        expect(JSON.stringify(result)).toBe(JSON.stringify(expected));
+    });
 
     test.each([
         {head:new ListNode(1, null), n:0, expected:null},
@@ -30,21 +45,6 @@ describe('LinkedListHelper class', () => {
     
         // Act
         let result = LinkedListHelper.removeNthNodeFromEnd(head, n);
-    
-        // Assert
-        expect(JSON.stringify(result)).toBe(JSON.stringify(expected));
-    });
-    
-    test.each([
-        {head:[], expected:null},
-        {head:[1], expected:new ListNode(1, null)},
-        {head:[1,2], expected:new ListNode(1, new ListNode(2, null))},
-        {head:[1,2,3], expected:new ListNode(1, new ListNode(2,  new ListNode(3, null)))},
-    ])('LinkedListHelper.fromArray()', ({head, expected})  => {
-        // Arrange
-    
-        // Act
-        let result = LinkedListHelper.fromArray(head);
     
         // Assert
         expect(JSON.stringify(result)).toBe(JSON.stringify(expected));
