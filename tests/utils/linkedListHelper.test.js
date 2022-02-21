34be6abda42d06dc7e34eae3dd5bb7b650c76136
Karn Ratana <karn@ratana.info>
2022-02-20 23:26:47 -0500

    Add LinkedListHelper.fromArray() tests

--- tests/utils/linkedListHelper.test.js
+++ tests/utils/linkedListHelper.test.js
@@ -5,6 +5,22 @@ describe('LinkedListHelper class', () => {
 
     
     test.each([
+        {head:[], expected:null},
+        {head:[1], expected:new ListNode(1, null)},
+        {head:[1,2], expected:new ListNode(1, new ListNode(2, null))},
+        {head:[1,2,3], expected:new ListNode(1, new ListNode(2,  new ListNode(3, null)))},
+    ])('LinkedListHelper.fromArray()', ({head, expected})  => {
+        // Arrange
+    
+        // Act
+        let result = LinkedListHelper.fromArray(head);
+    
+        // Assert
+        expect(JSON.stringify(result)).toBe(JSON.stringify(expected));
+    });
+
+    
+    test.each([
         {head:[], expected:""},
         {head:[1], expected:"1,"},
         {head:[1,2], expected:"1,2,"},
