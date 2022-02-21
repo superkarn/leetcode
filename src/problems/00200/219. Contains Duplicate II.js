ac12da2da0217a9c38f6e917aba8251345920ceb
Karn Ratana <karn@ratana.info>
2022-02-21 12:15:35 -0500

    Optimize brute force by only checking j up to i+1+k, and remove unnecessary check for Math.abs(i-j)

--- src/problems/00200/219. Contains Duplicate II.js
+++ src/problems/00200/219. Contains Duplicate II.js
@@ -30,9 +30,8 @@ var containsNearbyDuplicate = function(nums, k) {
 // Use brute force and check all numbers via nested loops
 let bruteForce = (nums, k) => {
     for (let i=0; i<nums.length; i++) {
-        for (let j=i+1; j<nums.length; j++) {
-            //console.log(`  [${i}](${nums[i]}) vs [${j}](${nums[j]}) <= ? ${k}`);
-            if ((nums[i] == nums[j]) && Math.abs(i-j) <= k) {
+        for (let j=i+1; j<nums.length && j<i+1+k; j++) {
+            if (nums[i] == nums[j]) {
                 return true;
             }
         }
