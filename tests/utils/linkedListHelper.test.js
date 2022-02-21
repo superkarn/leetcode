2457b192ad3e3de63e9c08b628b818bd092c239f
Karn Ratana <karn@ratana.info>
2022-02-20 23:47:54 -0500

    Add LinkedListHelper.removeNthNodeFromEnd() tests

--- tests/utils/linkedListHelper.test.js
+++ tests/utils/linkedListHelper.test.js
@@ -2,6 +2,22 @@ const ListNode = require('../../src/models/ListNode');
 const LinkedListHelper = require('../../src/utils/linkedListHelper');
 
 describe('LinkedListHelper class', () => {
+
+    test.each([
+        {head:new ListNode(1, null), n:1, expected:null},
+        {head:new ListNode(1, new ListNode(2, null)), n:1, expected:new ListNode(1, null)},
+        {head:new ListNode(1, new ListNode(2,  new ListNode(3, null))), n:1, expected:new ListNode(1, new ListNode(2,  null))},
+        {head:new ListNode(1, new ListNode(2,  new ListNode(3, null))), n:2, expected:new ListNode(1, new ListNode(3,  null))},
+        {head:new ListNode(1, new ListNode(2,  new ListNode(3, new ListNode(4, null)))), n:4, expected:new ListNode(2, new ListNode(3,  new ListNode(4,  null)))},
+    ])('LinkedListHelper.removeNthNodeFromEnd()', ({head, n, expected})  => {
+        // Arrange
+    
+        // Act
+        let result = LinkedListHelper.removeNthNodeFromEnd(head, n);
+    
+        // Assert
+        expect(JSON.stringify(result)).toBe(JSON.stringify(expected));
+    });
     
     test.each([
         {head:[], expected:null},
