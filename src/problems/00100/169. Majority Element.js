dc6c622bb59e2c5419fe90253f9dd1865e9f2418
Karn Ratana <karn@ratana.info>
2022-03-01 22:27:16 -0500

    Add problem 169. Majority Element (#66)
    * Add boiler plate
    
    * Add bruteForce() solution
    
    * Add sortingSolution()
    
    * Add boyerMoore() solution

---
+++ src/problems/00100/169. Majority Element.js
@@ -0,0 +1,74 @@
+// https://leetcode.com/problems/majority-element/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let startTime = Date.now();
+    console.log('--------------------------');
+    console.log('--Parameters--------------');
+    console.log(`nums      :  ${nums}`);
+
+    // Pick the algorithm
+    console.log('--Logs--------------------');
+    let result = boyerMore(nums);
+    
+    let endTime = Date.now();
+    console.log('--Stats-------------------');
+    console.log('Memory    : ', process.memoryUsage().heapUsed);
+    console.log('Total time: ', endTime - startTime);
+
+    console.log('--Result------------------');
+    console.log(result);
+    console.log('--------------------------');
+
+    return result;
+};
+
+// Count each number's frequency.  Return the first one that crosses the threshold.
+let bruteForce = (nums) => {
+    const TARGET = Math.floor(nums.length/2) +1;
+    let frequencies = {};
+
+    for (let i=0; i<nums.length; i++) {
+        frequencies[nums[i]] = 1 + (frequencies[nums[i]] ? frequencies[nums[i]] : 0);
+
+        if (frequencies[nums[i]] >= TARGET) {
+            return nums[i];
+        }
+    }
+
+    return 0;
+};
+
+// Sort, then grab the middle element.
+// Not as efficient because sort is O(nlogn)
+let sortingSolution = (nums) => {
+    nums.sort((a,b) => a-b);
+    return nums[Math.floor(nums.length/2)];
+};
+
+// Boyer-Moore Voting Algorithm
+// https://www.cs.utexas.edu/~moore/best-ideas/mjrty/
+let boyerMore = (nums) => {
+    let count = 0;
+    let result = 0;
+
+    for (let i=0; i<nums.length; i++) {
+        if (count == 0) {
+            result = nums[i];
+        }
+
+        count += (nums[i] == result) ? 1 : -1;
+    }
+
+    return result;
+};
+
+let inputs = [
+    { nums:[3,2,3], output:3 },
+    { nums:[2,2,1,1,1,2,2], output:2 },
+];
+
+// Comparing scalar output
+console.log(inputs.map(x => majorityElement(x.nums) == x.output));
