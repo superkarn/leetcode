1fd9fc10108d133845fca86471ee661ff415f230
Karn Ratana <karn@ratana.info>
2022-03-02 20:30:33 -0500

    Add boiler plate

---
+++ src/problems/00100/104. Maximum Depth of Binary Tree.js
@@ -0,0 +1,49 @@
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    let startTime = Date.now();
+    console.log('--------------------------');
+    console.log('--Parameters--------------');
+    console.log(`root    :  ${root}`);
+
+    // Pick the algorithm
+    console.log('--Logs--------------------');
+    let result = bruteForce(root);
+    
+    let endTime = Date.now();
+    console.log('--Stats-------------------');
+    console.log('Memory    : ', process.memoryUsage().heapUsed);
+    console.log('Total time: ', endTime - startTime);
+
+    console.log('--Result------------------');
+    console.log(result);
+    console.log('--------------------------');
+
+    return result;
+};
+
+// attempt1: description on the algorithm
+let bruteForce = (root) => {
+    let result = root;
+
+    return result;
+};
+
+let inputs = [
+    { root:[3,9,20,null,null,15,7], output:3 },
+    { root:[1,null,2], output:2 },
+];
+
+// Comparing array/object output
+console.log(inputs.map(x => JSON.stringify(maxDepth(x.root)) === JSON.stringify(x.output)));
